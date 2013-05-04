f32a7eab26d3361a12fa756251d0b429ecdfd551
Darren Yin <darrenyin0110@yahoo.com>
2013-05-04 03:11:52 -0400

    I forgot to migrate?

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
+
+  validates_presence_of :commentor, :body
+
   belongs_to :post
   attr_accessible :body, :commentor
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
+
+  validates_presence_of :name, :title, :body
+
+  has_many :comments, :dependent => :destroy
   attr_accessible :body, :created_at, :name, :title
 end
